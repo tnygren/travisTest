d640c5731e5b211fb13043d7dccaa16432b459ae
Teppo Nygren <teppo.nygren@iki.fi>
2016-05-06 22:27:25 +0300

    Test

--- src/cc/engeld/HelloWorld.java
+++ src/cc/engeld/HelloWorld.java
@@ -2,6 +2,6 @@ package cc.engeld;
 
 public class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("Hello World, another test again!");
+        System.out.println("Hello World, another test!");
     }
 }
