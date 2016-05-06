76cc7e7730a9c93840f6b89931d777c204187673
Teppo Nygren <teppo.nygren@iki.fi>
2016-05-06 21:05:59 +0300

    Making a bug

--- src/cc/engeld/HelloWorld.java
+++ src/cc/engeld/HelloWorld.java
@@ -1,6 +1,6 @@
 package cc.engeld;
 
-public class HelloWorld {
+public class HelloWorldBUG {
     public static void main(String[] args) {
         System.out.println("Hello World, I'm using Slack too!");
     }
